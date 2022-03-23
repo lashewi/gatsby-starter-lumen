d1387fc4795ff6d8d6b3626f68b0365301dbb574
lashew <swisum@gmail.com>
2022-03-23 22:22:03 +0530

    feat: twitter fix

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -5,23 +5,22 @@ import styles from './Author.module.scss';
 import { useSiteMetadata } from '../../../hooks';
 
 const Author = () => {
-  const { author } = useSiteMetadata();
+    const { author } = useSiteMetadata();
 
-  return (
-    <div className={styles['author']}>
-      <p className={styles['author__bio']}>
-        {author.bio}
-        <a
-          className={styles['author__bio-twitter']}
-          href={getContactHref('twitter', author.contacts.twitter)}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <strong>{author.name}</strong> on Twitter
-        </a>
-      </p>
-    </div>
-  );
+    return ( <
+        div className = { styles['author'] } >
+        <
+        p className = { styles['author__bio'] } > { author.bio } <
+        a className = { styles['author__bio-twitter'] }
+        href = { getContactHref('twitter', "lashewi") }
+        rel = "noopener noreferrer"
+        target = "_blank" >
+        <
+        strong > { author.name } < /strong> on Twitter < /
+        a > <
+        /p> < /
+        div >
+    );
 };
 
 export default Author;
